e8094a277a7f306d3711bd9523e7834f8187074d
Kassisong <64343603+Kassisong@users.noreply.github.com>
2023-08-31 18:33:21 +0800

    _02_2country_Pie_V2.py
    more sophisticated in figure

--- _02_2country_Pie.py
+++ _02_2country_Pie.py
@@ -1,73 +1,91 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan  4 16:00:33 2023
-
-@author: songyining
-"""
-
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.colors as colors
 import numpy as np
+import kaleido
+import plotly
+plotly.offline.init_notebook_mode(connected=True)
 
 
-
-plt.figure(figsize=(10,6),dpi=500)
+fig = go.Figure()
 
 def make_autopct(values):
     def my_autopct(pct):
         total = sum(values)
         val = int(round(pct*total/100.0))
-        # 同时显示数值和占比的饼图
         return '{v:d}'.format(p=pct,v=val)
     return my_autopct
 
 col = []
 for i in np.arange(0,11):
-    col.append(plt.cm.Set3(i))
+    col.append(colors.qualitative.Set3[i])
 for i in np.arange(0,5):
-    col.append(plt.cm.Set3(i))
-# col.append(plt.cm.Set3(3))
+    col.append(colors.qualitative.Set2[i])
 
+country_name = ['USA', 'Canada', 
+                'England', 'Germany', 'Italy', 'Netherlands', 'France', 'Russia', 'Spain', 'Poland', 
+                'Australia', 
+                'China', 'India', 'Japan', 'South African']
+country_num2 = [477, 123, 
+                256, 229, 145, 135, 94, 79, 49, 34, 
+                217, 
+                125, 70, 58, 
+                39]
 
-country_name=['USA','Canada',
-              'England','Germany','Italy','Netherlands','France','Russia','Spain','Poland',
-              'Australia',
-              'China','India','Japan',
-              'South African']
-country_num2 = [478,123,
-                256,229,145,135,94,79,49,34,
-                220,
-                125,72,58,
-                39]         
+continent = [600, 1021, 217, 253, 39]
+continent_name = ['North America', 'Europe', 'Oceania', 'Asia', 'African']
 
-continent=[601, 1021, 220, 255,39]
-continent_name = ['North America','Europe','Oceania','Asia','African']
+fig.data=[]
+fig.add_trace(
+    go.Pie(
+        labels=country_name,
+        values=country_num2,
+        sort=False,
+        marker=dict(colors=col),
+        textinfo='label+value',
+        hole=0.6,
+        hoverinfo='none',
+        showlegend=True,
+        textposition='inside',
+        name='Country',
+        rotation=9.6,
+        domain={'x': [0.12, 0.68], 'y':[0.15, 0.85] }
+    )
+)
 
-plt.title('The article volume of countries',fontsize=20,fontweight='bold')
-size = 0.3
-p1 = plt.pie(country_num2,
-             autopct=make_autopct(country_num2),
-             radius=1-size,
-             wedgeprops=dict(width=0.3),
-             colors=col,
-             labels=country_name,
-             pctdistance = 0.75,
-             labeldistance = 0.9,
-             textprops={'fontsize': 8})
+fig.add_trace(
+    go.Pie(
+        labels=continent_name,
+        values=continent,
+        sort=False,
+        marker=dict(colors=colors.sequential.Blues),
+        textinfo='label+value',
+        textfont=dict(color='navy'),
+        hole=0.7,
+        hoverinfo='none',
+        showlegend=True,
+        name='Continent',
+        rotation=-11,
+        domain={'x': [0, 0.8], 'y': [0, 1]}
+    )
+)
 
+fig.update_layout(
+    # title='The article volume of countries',
+    # title_x=0.5,
+    # title_font=dict(size=20),
+    legend=dict(
+        x=0.9,  # 调整x值以将图例显示在右边
+        y=0.5,
+        bgcolor='rgba(0,0,0,0)',
+        bordercolor='rgba(0,0,0,0)',
+#         font=dict(size=8),
+    ),
+    width=800,
+    height=600,
+    margin=dict(t=5, b=5,l=5,r=5)
+)
 
-color = plt.get_cmap('Blues')(np.linspace(0.8,0.2,len(continent)))
-p2 = plt.pie(continent,
-             autopct=make_autopct(continent),
-             radius=1,
-             wedgeprops=dict(width=0.3),
-             colors=color,
-             labels=continent_name,
-             pctdistance = 0.85,
-             labeldistance = 1.05,
-             textprops={'color': 'navy','fontsize': 15})
+fig.show()
 
-
-plt.legend(loc=0,bbox_to_anchor=(-0.05, 0.99),frameon=False,fontsize=8)
-
-plt.savefig('2-1.eps', format='eps',bbox_inches='tight')
+import plotly.io as pio
+pio.write_image(fig, 'plot.pdf', format='pdf')
